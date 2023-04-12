53e971afc9818329b27bfcb483cdb4c2566ac9eb
Yan Yan <evitayan@google.com>
2023-04-12 22:56:19 +0000

    fog: Add feature flag for IPSEC_TUNNEL_MIGRATION
    This commit adds the IPSEC_TUNNEL_MIGRATION feature flag,
    with XFRM_MIGRATE used as the implementation
    
    Bug: 273379472
    Test: build, flash and verify with "adb shell pm list features"
    Test: atest IpSecManagerTunnelTest
    Change-Id: Id2ee3e65183e32ea63f6251ba921e1e4c6b09ed0

--- device.mk
+++ device.mk
@@ -309,7 +309,8 @@ PRODUCT_PACKAGES += \
     android.system.net.netd@1.1.vendor
 
 PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.software.ipsec_tunnels.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.ipsec_tunnels.xml
+    frameworks/native/data/etc/android.software.ipsec_tunnels.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.ipsec_tunnels.xml \
+    frameworks/native/data/etc/android.software.ipsec_tunnel_migration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.ipsec_tunnel_migration.xml
 
 # Neural Networks
 PRODUCT_PACKAGES += \
