1ee8794d589e820c2848b75da5f30516d2d94ce0
angelomds42 <angelomds42@gmail.com>
2024-10-16 22:09:01 -0300

    fog: Build QTI USB Gadget HAL
    Signed-off-by: angelomds42 <angelomds42@gmail.com>
    Signed-off-by: An. <noegrahachan@gmail.com>

--- device.mk
+++ device.mk
@@ -475,7 +475,8 @@ PRODUCT_PACKAGES_DEBUG += \
 
 # USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.3-service.basic
+    android.hardware.usb@1.3-service.basic \
+    android.hardware.usb.gadget-service.qti
 
 PRODUCT_PACKAGES += \
     init.qcom.usb.rc \
