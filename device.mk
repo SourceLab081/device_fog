9a979a888e3d24002e4ac8b451fa270ffa8401be
sources labs <7871884-sourceslab062@users.noreply.gitlab.com>
2025-02-05 11:18:02 +0000

    remove signing

--- device.mk
+++ device.mk
@@ -445,9 +445,6 @@ PRODUCT_SOONG_NAMESPACES += \
     hardware/lineage/interfaces/power-libperfmgr \
     hardware/xiaomi
 
-# Signing
-include vendor/pixelage-priv/keys/keys.mk
-
 # Update engine
 PRODUCT_PACKAGES += \
     update_engine \
