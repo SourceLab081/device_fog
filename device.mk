760e313c84e267629ed309caeed6144e56fb7059
sources labs <7871884-sourceslab062@users.noreply.gitlab.com>
2025-02-07 16:32:40 +0000

    remove WfdCommon

--- device.mk
+++ device.mk
@@ -553,9 +553,6 @@ PRODUCT_PACKAGES += \
     libnl \
     libwfdaac_vendor
 
-PRODUCT_BOOT_JARS += \
-    WfdCommon
-
 # WiFi firmware symlinks
 PRODUCT_PACKAGES += \
     firmware_wlan_mac.bin_symlink \
