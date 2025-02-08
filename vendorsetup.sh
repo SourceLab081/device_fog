19b10be70209b3370d0f976f23315cc33ec46464
sources labs <7871884-sourceslab062@users.noreply.gitlab.com>
2025-02-08 14:35:35 +0000

    update

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,6 +1,6 @@
 # Remove Pixel kernel header
 rm hardware/google/pixel/kernel_headers/Android.bp
-
+rm -rf packages/resources/devicesettings && git clone https://github.com/LineageOS/android_packages_resources_devicesettings --depth 1 -b lineage-20.0 packages/resources/devicesettings
 # Clone vendor tree
 #git clone -b lineage-21-staging https://github.com/alternoegraha/vendor_xiaomi_fog vendor/xiaomi/fog
 
@@ -11,7 +11,5 @@ rm hardware/google/pixel/kernel_headers/Android.bp
 #git clone https://github.com/LineageOS/android_hardware_xiaomi hardware/xiaomi
 
 # Kernel fixups
-# cd kernel/xiaomi/fog
 rm kernel/xiaomi/fog/Android.bp
 rm kernel/xiaomi/fog/techpack/audio/Android.bp
-# cd ../../..
