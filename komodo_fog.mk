7fb041337d456109069cb528b9bc2541ac1e6599
sources labs <7871884-sourceslab062@users.noreply.gitlab.com>
2025-02-15 03:24:16 +0000

    komodo Time

--- komodo_fog.mk
+++ komodo_fog.mk
@@ -8,16 +8,28 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit from common DerpFest-AOSP configuration
-$(call inherit-product, vendor/derp/config/common_full_phone.mk)
-TARGET_BOOT_ANIMATION_RES := 720
-TARGET_FACE_UNLOCK_SUPPORTED := true
-TARGET_NOT_USES_BLUR := true
+# Inherit some common Komodo-OS stuff.
+$(call inherit-product, vendor/komodo/config/common.mk)
 
 # Inherit from fog device
 $(call inherit-product, device/xiaomi/fog/device.mk)
 
-PRODUCT_NAME := derp_fog
+# Komodo Stuff
+KOMODO_OFFICIAL := false
+KOMODO_GAPPS_TYPE := gapps
+KOMODO_VARIANT := RELEASE
+
+TARGET_BOOT_ANIMATION_RES := 720
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_USES_BLUR := true
+TARGET_USES_AOSP_CHARGER := true
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
+TARGET_INCLUDE_LIVE_WALLPAPERS := false
+TARGET_INCLUDE_STOCK_ARCORE := false
+
+
+
+PRODUCT_NAME := komodo_fog
 PRODUCT_DEVICE := fog
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := Redmi
