9b934c8a7f584957c813c6a13018fd54a48937bb
online0code <151095682+online0code@users.noreply.github.com>
2025-03-08 11:43:05 +0700

    Update horizon_fog.mk

--- horizon_fog.mk
+++ horizon_fog.mk
@@ -12,11 +12,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, vendor/horizon/config/common_full_phone.mk)
 
 TARGET_FACE_UNLOCK_SUPPORTED := true
-TARGET_SUPPORTS_QUICK_TAP := false
+TARGET_SUPPORTS_QUICK_TAP := true
 TARGET_ENABLE_BLUR := false
-
-# Gapps
-WITH_GMS := false
 
 TARGET_BOOT_ANIMATION_RES := 720
 HORIZON_MAINTAINER := sourceslab062_gitlab
