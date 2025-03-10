0115f503001974fee626bcb0cf32232d94bc7a5e
Nolen Johnson <johnsonnolen@gmail.com>
2025-03-10 15:36:30 -0400

    fog: Migrate away from TARGET_RECOVERY_DEVICE_MODULES
    * Dead as of QPR2.
    
    Change-Id: I05bcf246266b89fa7da964f7aa75870b5a63a3b9
    Signed-off-by: An. <noegrahachan@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -82,7 +82,6 @@ HWUI_COMPILE_FOR_PERF := true
 
 # Init
 TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):libinit_fog
-TARGET_RECOVERY_DEVICE_MODULES := libinit_fog
 
 # Kernel
 BOARD_KERNEL_BASE        := 0x00000000
