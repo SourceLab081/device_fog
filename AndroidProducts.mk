ab7731e33cf91eab645d60add51a92a9d53e9359
online0code <151095682+online0code@users.noreply.github.com>
2025-03-13 15:17:02 +0000

    Adapt to LMODroid

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,8 +1,8 @@
 #
-# Copyright (C) 2022 The LineageOS Project
+# Copyright (C) 2022 The lmodroidOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_fog.mk
+    $(LOCAL_DIR)/lmodroid_fog.mk
