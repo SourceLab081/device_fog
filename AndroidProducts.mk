0df73cb748f703383d37b7b2750845a6f98ca3ee
online0code <151095682+online0code@users.noreply.github.com>
2025-03-14 11:31:20 +0700

    revert

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,10 +1,15 @@
 #
-# Copyright (C) 2022 The LineageOS Project
+# Copyright (C) 2022 The horizonOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/horizon_fog.mk
+
+COMMON_LUNCH_CHOICES := \
+     horizon_fog-user \
+     horizon_fog-userdebug \
+     horizon_fog-eng
 
 
