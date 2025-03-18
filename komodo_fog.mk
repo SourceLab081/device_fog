7edbb4d3bb100d16a82ad20b5e55b9a00060745f
online0code <151095682+online0code@users.noreply.github.com>
2025-03-18 14:36:21 +0000

    Komodo Time

--- komodo_fog.mk
+++ komodo_fog.mk
@@ -8,23 +8,29 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common DerpFest stuff.
-$(call inherit-product, vendor/derp/config/common_full_phone.mk)
-TARGET_NOT_USES_BLUR := true
-TARGET_SUPPORTS_QUICK_TAP := false
-DERP_VERSION_APPEND_TIME_OF_DAY := true
+# Inherit some common komodo stuff.
+$(call inherit-product, vendor/komodo/config/common_full_phone.mk)
+
+# Komodo Stuff
+KOMODO_OFFICIAL := false
+KOMODO_GAPPS_TYPE := gapps
+KOMODO_VARIANT := RELEASE
 
 TARGET_BOOT_ANIMATION_RES := 720
 TARGET_FACE_UNLOCK_SUPPORTED := true
-TARGET_SUPPORTS_QUICK_TAP := false
-
+TARGET_USES_BLUR := true
+TARGET_USES_AOSP_CHARGER := true
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
+TARGET_INCLUDE_LIVE_WALLPAPERS := false
+TARGET_INCLUDE_STOCK_ARCORE := false
+WITH_GAPPS := true
 # Gapps
-TARGET_USES_MINI_GAPPS := true
+TARGET_USES_PICO_GAPPS := true
 
 # Inherit from fog device
 $(call inherit-product, device/xiaomi/fog/device.mk)
 
-PRODUCT_NAME := derp_fog
+PRODUCT_NAME := komodo_fog
 PRODUCT_DEVICE := fog
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := Redmi
