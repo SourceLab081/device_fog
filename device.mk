09a1ee46a3c59ffa565459f049416e10a6799b6b
Yumi Yukimura <me.cafebabe@gmail.com>
2025-04-07 07:00:19 +0000

    fog: Disable UFFD GC via OVERRIDE_ENABLE_UFFD_GC * Our kernel (4.19) does not support features needed for UFFD GC.   Leaving it enabled results in long waiting time on the first boot.
    Change-Id: Ia8a17467e13d343c03ee29e514994946e7e84985
    Signed-off-by: Arijit78 <sahaarijit2007@gmail.com>
    Signed-off-by: alternoegraha <noegrahachan@gmail.com>

--- device.mk
+++ device.mk
@@ -523,6 +523,9 @@ endif
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/init/fstab.qcom:$(TARGET_COPY_OUT_VENDOR_RAMDISK)/first_stage_ramdisk/fstab.qcom
 
+# UFFD GC
+OVERRIDE_ENABLE_UFFD_GC := false
+
 # Vendor service manager
 PRODUCT_PACKAGES += \
     vndservicemanager
