fbf577175b41b2ae29cdebe2cea1942f07c6a9a4
online0code <151095682+online0code@users.noreply.github.com>
2025-04-14 21:00:43 +0000

    add codes for fixing the error

--- hals/display/Android.mk
+++ hals/display/Android.mk
@@ -1,6 +1,9 @@
 sdm-libs := sdm/libs
 display-hals := include $(sdm-libs)/utils $(sdm-libs)/core libdebug 
 
+display-hals += sde-drm
+display-hals += libdrmutils
+
 ifeq ($(call is-vendor-board-platform,QCOM),true)
     include $(call all-named-subdir-makefiles,$(display-hals))
 else
