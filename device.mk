b8cce0be4933601c2af452ce4066e6198f4fa680
online0code <151095682+online0code@users.noreply.github.com>
2025-04-24 11:15:38 +0700

    Update device.mk fix conflict

--- device.mk
+++ device.mk
@@ -214,10 +214,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
     android.hidl.base@1.0.vendor \
-    libhidltransport.vendor \
-    libhwbinder.vendor
+    libhidltransport.vendor
+#\    libhwbinder.vendor
 
-PRODUCT_COPY_FILES += \
+# PRODUCT_COPY_FILES += \
     prebuilts/vndk/v32/arm64/arch-arm-armv8-a/shared/vndk-sp/libhidlbase.so:$(TARGET_COPY_OUT_VENDOR)/lib/libhidlbase-v32.so \
     prebuilts/vndk/v32/arm64/arch-arm64-armv8-a/shared/vndk-sp/libhidlbase.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libhidlbase-v32.so
 
