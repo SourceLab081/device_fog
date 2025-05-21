0dda6a7cf01dd3d6b2c0ffc6a33a402d0c01b1ed
An. <noegrahachan@gmail.com>
2025-05-21 13:56:33 +0000

    fog: Include GMS if available
    Signed-off-by: An. <noegrahachan@gmail.com>

--- lineage_fog.mk
+++ lineage_fog.mk
@@ -12,6 +12,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 TARGET_BOOT_ANIMATION_RES := 720
 
+# Inherit GMS if available
+$(call inherit-product-if-exists, vendor/gapps/arm64/arm64-vendor.mk)
+
 # Inherit from fog device
 $(call inherit-product, device/xiaomi/fog/device.mk)
 
