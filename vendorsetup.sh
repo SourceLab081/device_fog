50852be042c6cbbb7bf2e21aad87ebb57184da16
Yograt <fedrix56@gmail.com>
2025-08-01 14:53:00 +0700

    fog: update cloning links and branches
    Signed-off-by: Yograt <fedrix56@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -2,10 +2,10 @@
 rm -rf hardware/google/pixel/kernel_headers/Android.bp
 
 # Clone vendor tree
-git clone -b fifteen-qpr2 https://github.com/alternoegraha/vendor_xiaomi_fog vendor/xiaomi/fog
+git clone -b fifteen-qpr2 https://github.com/yograts-playground/vendor_xiaomi_fog vendor/xiaomi/fog --depth 1
 
 # Clone prebuilt kernel
-git clone https://github.com/alternoegraha/device_xiaomi_fog-kernel device/xiaomi/fog-kernel
+git clone https://github.com/alternoegraha/device_xiaomi_fog-kernel device/xiaomi/fog-kernel --depth 1
 
 # Clone LineageOS' hardware/xiaomi
-git clone -b lineage-22.2 https://github.com/LineageOS/android_hardware_xiaomi hardware/xiaomi
+git clone -b lineage-23 https://github.com/LineageOS/android_hardware_xiaomi hardware/xiaomi --depth 1
