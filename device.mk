4b432a48426b9e19e416c9f08cb65d102d09a8a4
online0code <151095682+online0code@users.noreply.github.com>
2025-09-08 18:35:40 +0700

    Fix error from kernel local entry

--- device.mk
+++ device.mk
@@ -273,11 +273,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_ENABLE_UFFD_GC := true
 PRODUCT_SET_DEBUGFS_RESTRICTIONS := true
 
-# Kernel
-LOCAL_KERNEL := device/xiaomi/fog-kernel/Image
-PRODUCT_COPY_FILES += \
-	$(LOCAL_KERNEL):kernel
-
 # Keymaster
 PRODUCT_PACKAGES += \
     android.hardware.keymaster@4.1.vendor
