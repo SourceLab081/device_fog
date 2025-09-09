016344752e94ac4f19bbe9b6aac41c436ee3b8bb
online0code <151095682+online0code@users.noreply.github.com>
2025-09-09 12:36:27 +0700

    Remove miuicamera folder

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -6,6 +6,7 @@ rm kernel/xiaomi/fog/Android.bp
 rm kernel/xiaomi/fog/techpack/audio/Android.bp
 
 # Clone miuicamera
-if [ ! -d vendor/xiaomi/miuicamera ]; then
-   git clone -b 15 https://github.com/c0smic-Lab/vendor_xiaomi_miuicamera vendor/xiaomi/miuicamera --depth 1
-fi   
+# if [ ! -d vendor/xiaomi/miuicamera ]; then
+#   git clone -b 15 https://github.com/c0smic-Lab/vendor_xiaomi_miuicamera vendor/xiaomi/miuicamera --depth 1
+# fi   
+rm -rf vendor/xiaomi/miuicamera
