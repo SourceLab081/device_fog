33600faeb68e5f8a600b3fced534ed8a35cffc2e
online0code <151095682+online0code@users.noreply.github.com>
2025-09-10 10:51:05 +0700

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -4,3 +4,5 @@ rm hardware/google/pixel/kernel_headers/Android.bp
 # Kernel fixups
 rm kernel/xiaomi/fog/Android.bp
 rm kernel/xiaomi/fog/techpack/audio/Android.bp
+#The-Clover-Project/device_lineage_sepolicy
+git clone https://github.com/The-Clover-Project/device_lineage_sepolicy --depth 1 -b 16 device/lineage/sepolicy
