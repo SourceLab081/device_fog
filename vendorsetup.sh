0f1c2b8aa6263883509d2320f5612b650d3f1cfe
online0code gmail <craveio0explore@gmail.com>
2025-09-14 00:40:21 +0000

    Add KernelSU-Next

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -4,3 +4,5 @@ rm hardware/google/pixel/kernel_headers/Android.bp
 # Kernel fixups
 rm kernel/xiaomi/fog/Android.bp
 rm kernel/xiaomi/fog/techpack/audio/Android.bp
+curDir=`pwd`
+cd kernel/xiaomi/fog && rm -rf KernelSU-Next && curl -LSs "https://raw.githubusercontent.com/KernelSU-Next/KernelSU-Next/next/kernel/setup.sh" | bash - && cd $curDir
