8e57dbc67da666ccf672a9d5c862b34bd7debaa6
online0code gmail <craveio0explore@gmail.com>
2025-09-16 15:06:39 +0000

    Enable Gapps build & change kernel name

--- yaap_fog.mk
+++ yaap_fog.mk
@@ -12,7 +12,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 # Inherit some common YAAP stuff.
 $(call inherit-product, vendor/yaap/config/common_full_phone.mk)
 
-
+# Gapps
+TARGET_BUILD_GAPPS := true
 TARGET_BOOT_ANIMATION_RES := 720
 
 # Inherit GMS if available
