121a20215ab9a3a7709032c0e75a2fac16332b41
.:::01000101:::. <58969724+SourceLab081@users.noreply.github.com>
2025-12-06 22:17:34 +0700

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -4,3 +4,8 @@ rm  hardware/google/pixel/kernel_headers/Android.bp
 # Kernel fixups
 rm kernel/xiaomi/fog/Android.bp
 rm kernel/xiaomi/fog/techpack/audio/Android.bp
+
+if [ "$forHyb" = "yes" ]; then
+   wget https://github.com/SourceLab081/uploadz/releases/download/v0.2.1/BoardConfig.mk && mv BoardConfig.mk device/xiaomi/fog/
+   rm -rf kernel/xiaomi/fog && git clone https://github.com/SourceLab081/hybris_kernel -b sfos-fog --depth 1  kernel/xiaomi/fog
+fi
