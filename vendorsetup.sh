389964f3b8840c5b0a81c910bf9c93ed76aeda0c
.:::01000101:::. <58969724+SourceLab081@users.noreply.github.com>
2025-12-06 21:49:59 +0700

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -3,4 +3,4 @@ rm  hardware/google/pixel/kernel_headers/Android.bp
 
 # Kernel fixups
 rm kernel/xiaomi/fog/Android.bp
-rm kernel/xiaomi/fogtechpack/audio/Android.bp
+rm kernel/xiaomi/fog/techpack/audio/Android.bp
