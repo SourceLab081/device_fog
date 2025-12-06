d94c4b6a91ba57a1fe566432168ea5184827c3d8
.:::01000101:::. <58969724+SourceLab081@users.noreply.github.com>
2025-12-06 13:43:26 +0700

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,6 +1,6 @@
 # Remove Pixel kernel header
-rm -rf hardware/google/pixel/kernel_headers/Android.bp
+rm  hardware/google/pixel/kernel_headers/Android.bp
 
 # Kernel fixups
-rm /kernel/xiaomi/fog/Android.bp
-rm /kernel/xiaomi/fogtechpack/audio/Android.bp
+rm kernel/xiaomi/fog/Android.bp
+rm kernel/xiaomi/fogtechpack/audio/Android.bp
