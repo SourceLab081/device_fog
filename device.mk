9991f68e3cee63e265b7d26c5ef0f62b2bfa1107
.:::01000101:::. <58969724+SourceLab081@users.noreply.github.com>
2026-02-15 17:40:31 +0700

    Change target screen density from 280 to 400

--- device.mk
+++ device.mk
@@ -142,7 +142,7 @@ PRODUCT_DEX_PREOPT_DEFAULT_COMPILER_FILTER := speed-profile
 USE_DEX2OAT_DEBUG := false
 
 # Display
-TARGET_SCREEN_DENSITY := 280
+TARGET_SCREEN_DENSITY := 400
 
 PRODUCT_PACKAGES += \
     android.hardware.graphics.mapper@3.0-impl-qti-display \
