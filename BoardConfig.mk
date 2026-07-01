fa37f4aee1b03684bb744e3dc4ee403bd984c1e1
Nolen Johnson <johnsonnolen@gmail.com>
2026-07-01 01:34:49 -0400

    fog: Use legacy libion implementation
    * At least one blob directly depends on this, with
      many other bits and pieces likely.
    
    Change-Id: Ic063618d6942a4f6e90dd7e21698e36d14113d06

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -194,6 +194,7 @@ ENABLE_VENDOR_RIL_SERVICE := true
 VENDOR_SECURITY_PATCH := $(PLATFORM_SECURITY_PATCH)
 
 # Sepolicy
+include device/lineage/sepolicy/libion/sepolicy.mk
 include device/lineage/sepolicy/libperfmgr/sepolicy.mk
 include device/qcom/sepolicy_vndr/SEPolicy.mk
 
