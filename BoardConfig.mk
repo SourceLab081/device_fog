180a205047ee0c771c3dc8d1699ef2de6a8116d9
.::01000101::. <58969724+SourceLab081@users.noreply.github.com>
2026-08-24 10:06:14 +0000

    Revert "fog: Use legacy libion implementation"
    This reverts commit fa37f4aee1b03684bb744e3dc4ee403bd984c1e1.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -197,7 +197,6 @@ ENABLE_VENDOR_RIL_SERVICE := true
 VENDOR_SECURITY_PATCH := $(PLATFORM_SECURITY_PATCH)
 
 # Sepolicy
-include device/lineage/sepolicy/libion/sepolicy.mk
 include device/lineage/sepolicy/libperfmgr/sepolicy.mk
 include device/qcom/sepolicy_vndr/SEPolicy.mk
 
