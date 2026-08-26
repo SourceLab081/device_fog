PixelOS-Devices/android_device_xiaomi_miatoll

miatoll: Use legacy libion implementation
* At least one blob directly depends on this, with
  many other bits and pieces likely.

Change-Id: Ida76ff1e910c04ba04e59d02ae8b91041b825d96

BoardConfig.mk
@@ -153,6 +153,7 @@ ENABLE_VENDOR_RIL_SERVICE := true
 VENDOR_SECURITY_PATCH := 2023-05-01
 
 # Sepolicy
+include device/lineage/sepolicy/libion/sepolicy.mk
 include device/lineage/sepolicy/libperfmgr/sepolicy.mk
 include device/qcom/sepolicy_vndr/SEPolicy.mk
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor

SourceLab081/device_fog

Signed-off-by: iHSAN <ihsanulrahman@proton.me>

BoardConfig.mk
@@ -198,6 +198,7 @@ VENDOR_SECURITY_PATCH := $(PLATFORM_SECURITY_PATCH)
 
 # Sepolicy
 include device/voltage/sepolicy/libperfmgr/sepolicy.mk
+include device/voltage/sepolicy/libion/sepolicy.mk
 include device/qcom/sepolicy_vndr/SEPolicy.mk
 
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private