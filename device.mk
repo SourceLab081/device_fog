be60b47a9c03958247e48368fd932e651a9bc30e
.::01000101::. <58969724+SourceLab081@users.noreply.github.com>
2026-08-26 06:50:33 +0000

    Revert "fog: power-libperfmgr: Update included soong namespaces"
    This reverts commit 025fe3830a4a7a40023985084b13425fc82e1dc9.

--- device.mk
+++ device.mk
@@ -451,7 +451,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
     hardware/google/interfaces \
-    hardware/google/pixel/power-libperfmgr \
+    hardware/google/pixel \
     hardware/lineage/interfaces/power-libperfmgr \
     hardware/qcom-caf/common/libqti-perfd-client \
     hardware/xiaomi \
