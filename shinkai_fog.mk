9b4cbb27e2e02c62212f2a0c26e3601767846a82
.::01000101::. <58969724+SourceLab081@users.noreply.github.com>
2026-09-17 18:20:20 +0700

    fog: Adapt to Shinkai Project

--- shinkai_fog.mk
+++ shinkai_fog.mk
@@ -9,24 +9,18 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/custom/config/common_full_phone.mk)
 TARGET_BOOT_ANIMATION_RES := 720
 
 # Inherit from fog device
 $(call inherit-product, device/xiaomi/fog/device.mk)
 
-RISING_MAINTAINER="teleg3_7"
-# Gms 
-WITH_GMS := false
-# Ship Pixel Launcher
-TARGET_DEFAULT_PIXEL_LAUNCHER := false
-# disable/enable blur support, default is false
-# TARGET_ENABLE_BLUR := true
+TARGET_SUPPORTS_OMX_SERVICE := false
+SHINKAI_MAINTAINER=teleg3_7
 TARGET_FACE_UNLOCK_SUPPORTED := true
 TARGET_SUPPORTS_QUICK_TAP := true
-RISING_BUILDTYPE := UNOFFICIAL
 
-PRODUCT_NAME := lineage_fog
+PRODUCT_NAME := shinkai_fog
 PRODUCT_DEVICE := fog
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := Redmi
