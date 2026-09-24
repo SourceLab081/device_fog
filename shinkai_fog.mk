5ccd04ebf6ea949e8dd3d0309c11fab155ec0789
.::01000101::. <58969724+SourceLab081@users.noreply.github.com>
2026-09-24 12:56:42 +0700

    Update shinkai_fog.mk

--- shinkai_fog.mk
+++ shinkai_fog.mk
@@ -4,10 +4,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-
 # Inherit some common stuff.
 $(call inherit-product, vendor/custom/config/common_full_phone.mk)
 TARGET_BOOT_ANIMATION_RES := 720
@@ -15,10 +11,8 @@ TARGET_BOOT_ANIMATION_RES := 720
 # Inherit from fog device
 $(call inherit-product, device/xiaomi/fog/device.mk)
 
-TARGET_SUPPORTS_OMX_SERVICE := false
 SHINKAI_MAINTAINER := teleg3_7
 TARGET_FACE_UNLOCK_SUPPORTED := true
-TARGET_SUPPORTS_QUICK_TAP := false
 # This ROM cannot be set as a non-GApps ROM, but it may need to be configured for the partition.
 WITH_GMS := true
 
