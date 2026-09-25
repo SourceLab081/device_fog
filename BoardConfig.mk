2244411c199bf89125a2b595cde4b72c11d65018
.::01000101::. <58969724+SourceLab081@users.noreply.github.com>
2026-09-25 22:30:26 +0700

    Add flags to bypass hidden API checks

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -9,6 +9,9 @@ DEVICE_PATH := device/xiaomi/fog
 # Izinkan build melanjut walau ada modul opsional bawaan AOSP yang hilang
 PRODUCT_ALLOW_MISSING_OPTIONAL_BUILD_MODULES := true
 
+# Bypass validasi daftar API tersembunyi (Hidden API Check)
+UNSAFE_DISABLE_HIDDENAPI_FLAGS := true
+
 # A/B
 BOARD_USES_RECOVERY_AS_BOOT := true
 AB_OTA_UPDATER := true
